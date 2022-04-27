1f54a580338fe4c551bcc9139a26c7e79305d49e
Carlos Portella <carlosportella16@hotmail.com>
2022-04-27 14:11:38 -0300

    User update with PUT

--- src/main/java/com/course/workshopmongo/resources/UserResource.java
+++ src/main/java/com/course/workshopmongo/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,5 +65,14 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 
+	}
+	
+	// Update by ID
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/course/workshopmongo/services/UserService.java
+++ src/main/java/com/course/workshopmongo/services/UserService.java
@@ -13,30 +13,42 @@ import com.course.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
-		return user.orElseThrow(() -> 
-		new ObjectNotFoundException("Object not found"));
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
-	
+
 	public User isert(User obj) {
 		return repo.insert(obj);
 	}
-	
-	public User fromDTO(UserDTO objDto) {
-		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
-	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
 }
